c439a51d9457e93cad3bcdc0535b23a1b4c8fb72
prodillo <prodillo@gmail.com>
2017-10-06 13:05:08 -0700

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,33 +1,26 @@
+# Your task is to make sure that this mapper code does not fail on corrupt data lines,
+# but instead just ignores them and continues working
 import sys
 
-salesTotal = 0
-oldKey = None
+def mapper():
+    # read standard input line by line
+    for line in sys.stdin:
+        # strip off extra whitespace, split on tab and put the data in an array
+        data = line.strip().split("\t")
 
-for line in sys.stdin:
-
-    data = line.strip().split("\t")
-    if len(data) != 2:
-
-        # Something has gone wrong. Skip this line.
-
-        continue
-
-    thisKey, thisSale = data
-
-    if oldKey and oldKey != thisKey:
-
-      print oldKey, "\t", salesTotal
-
-      oldKey = thisKey
-
-      salesTotal = 0
-
-     oldKey = thisKey
-
-     salesTotal += float(thisSale)
-
-
-
- if oldKey != None:
-
-   print oldKey, "\t", salesTotal
+        # This is the place you need to do some defensive programming
+        # what if there are not exactly 6 fields in that line?
+        # YOUR CODE HERE
+        
+        if len(data)==6:
+        
+        
+            # this next line is called 'multiple assignment' in Python
+            # this is not really necessary, we could access the data
+            # with data[2] and data[5], but we do this for conveniency
+            # and to make the code easier to read
+            date, time, store, item, cost, payment = data
+        
+            # Now print out the data that will be passed to the reducer
+            print "{0}\t{1}".format(item, cost)
+        
